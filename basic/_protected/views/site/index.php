f7858ef7e17c7fd67f7fa9182d2f9fc5f710e56c
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-06-29 08:28:13 +0500

    sas
    asa

--- basic/_protected/views/site/index.php
+++ basic/_protected/views/site/index.php
@@ -369,7 +369,9 @@ use yii\bootstrap\Modal;
             <div class="row" data-aos="fade-up">
 
                 <div class="col-lg-6 ">
-                    <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+                    <iframe class="mb-4 mb-lg-0"
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5415.278233154889!2d72.3458107!3d40.7514471!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQ1JzE3LjkiTiA3MsKwMjAnNDcuNSJF!5e1!3m2!1sru!2s!4v1593400268382!5m2!1sru!2s"
+                            frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
                 </div>
 
                 <div class="col-lg-6">
