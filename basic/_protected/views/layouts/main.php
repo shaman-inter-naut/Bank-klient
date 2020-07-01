91dd3490323348370be2b1856c37c1c7c7e4c2b2
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-07-01 13:36:02 +0500

    sad
    asd

--- basic/_protected/views/layouts/main.php
+++ basic/_protected/views/layouts/main.php
@@ -32,6 +32,7 @@ AppAsset::register($this);
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
         <link href="themes/day/assets/css/style.css" rel="stylesheet">
 
+
         <!--<link rel="stylesheet" href="../../web/new.css"> -->
 
         <?php $this->head() ?>
