0e93442c238e39401415d07e7d7c52c2412c07d0
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-07-03 18:39:10 +0500

    asdsa
    da

--- basic/_protected/views/accountnumber/update.php
+++ basic/_protected/views/accountnumber/update.php
@@ -5,14 +5,15 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\AccountNumber */
 
-$this->title = 'Update Account Number: ' . $model->id;
+//$this->title = 'Update Account Number: ' . $model->id;
+$this->title = 'Ўзгартириш: ';
 $this->params['breadcrumbs'][] = ['label' => 'Account Numbers', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="account-number-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h5><?= Html::encode($this->title) ?></h5>
 
     <?= $this->render('_form', [
         'model' => $model,
