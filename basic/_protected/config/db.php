8afee96daf3df43421464ed29d866f52d2ce651b
shaman-inter-naut <65750511+shaman-inter-naut@users.noreply.github.com>
2020-07-07 15:32:13 +0500

    ty
    rtyrtyr

--- basic/_protected/config/db.php
+++ basic/_protected/config/db.php
@@ -1,18 +1,18 @@
 <?php
 
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=10.142.61.180;dbname=bank_client',
-//    'username' => 'bank_client',
-//    'password' => 'bank_client',
-//    'charset' => 'utf8',
-//];
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bank_client',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=10.142.61.180;dbname=bank_client',
+    'username' => 'bank_client',
+    'password' => 'bank_client',
     'charset' => 'utf8',
 ];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=bank_client',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
+//];
 
